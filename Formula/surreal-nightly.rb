b75479e60ee7e772849e6a81bdcad6a655c29e40
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-26 21:33:33 +0100

    Add nightly release

---
+++ Formula/surreal-nightly.rb
@@ -0,0 +1,57 @@
+class Surreal < Formula
+  desc "A scalable, distributed, collaborative, document-graph database"
+  homepage "https://surrealdb.com"
+
+  version "nightly"
+  url "https://download.surrealdb.com/nightly/surreal-nightly.darwin-universal.tgz"
+
+  def install
+    bin.install "surreal-nightly"
+  end
+
+  def caveats; <<~EOS
+    For local development only, this formula ships a launchd config
+    to start a single-node cluster that stores its data under:
+      #{var}/
+    The database is available on the default port of 8000:
+      #{Formatter.url("http://localhost:8000")}
+  EOS
+  end
+
+  plist_options :manual => "surreal-nightly start --user root --pass root --log debug memory"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/surreal</string>
+        <string>start</string>
+        <string>--user</string>
+        <string>root</string>
+        <string>--pass</string>
+        <string>root</string>
+        <string>--log</string>
+        <string>debug</string>
+        <string>file://#{var}/surreal.db</string>
+      </array>
+      <key>WorkingDirectory</key>
+      <string>#{var}</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "#{bin}/surreal-nightly version"
+  end
+
+end
