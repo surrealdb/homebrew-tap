89c27a63d802c40ba3a0cc08348e81784543ee28
Rushmore Mushambi <rushmore@surrealdb.com>
2026-06-04 09:57:59 +0100

    Upgrade to v3.1.3

--- Formula/surreal.rb
+++ Formula/surreal.rb
@@ -3,9 +3,9 @@ class Surreal < Formula
   desc "A scalable, distributed, collaborative, document-graph database"
   homepage "https://surrealdb.com"
 
-  version "3.1.2"
-  url "https://download.surrealdb.com/v3.1.2/surreal-v3.1.2.darwin-universal.tgz"
-  sha256 "bfec2d766fb19efdcd1c53e719ea8a4097d02bac6b3269f0b43fb08b7d9a44be"
+  version "3.1.3"
+  url "https://download.surrealdb.com/v3.1.3/surreal-v3.1.3.darwin-universal.tgz"
+  sha256 "330a13dda077cb964fd9fa7377e48b6a670d430bccfeaae856142ad8c1cc89b7"
 
   def install
     bin.install "surreal"
